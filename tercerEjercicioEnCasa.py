9bbee8bb838d9d4cc9d246ebe85d0afca0aba278
David3743 <115675224+David3743@users.noreply.github.com>
2022-11-03 18:01:48 -0500

    Add files via upload
    Ejercico de las diapositivas

---
+++ tercerEjercicioEnCasa.py
@@ -0,0 +1,7 @@
+#Hacer un programa para hacer gestión de estudiantes utilizando como contenedor un diccionario.
+# Los atributos de un estudiante son: código, nombre, apellidos, y tres notas. Debe permitir agregar,
+# modificar, eliminar, ver alguno y ver todos, además algunas estadísticas como promedios –listado-,
+# promedio de un estudiante, promedio general, el mejor estudiante (con toda su información incluido promedio)
+# y el peor estudiante (ídem).​
+estudiantes ={"codigo":[101,102,103,104,105,106,107,108,109,110], "nombre":["Juan","Camilo","Eduard","Pablo","David","Gabriela","Daniel","Daniela","María","Maya"], "apellidos":["Lopez","Jimenez","Meza","Flores","Vargas","Montero","Mejia","Mendoza","Marquez","Garcia"] , "nota1":[1.0,2.0,3.0,4.0,5.0,4.5,3.5,2.5,1.5,0,0.5] ,"nota2":[1.2,2.2,3.3,4.5,3.6,3.8,4.7,4.9,1.2,1.9] ,"nota3":[1.5,4.7,2.5,2.5,2.9,2.9,4.3,3.4,3.6]}
+print(estudiantes)
